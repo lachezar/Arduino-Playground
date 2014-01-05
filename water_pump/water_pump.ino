b7994218bef8bc739ce6ca834f8068aca15f7f7a
Lachezar Yankov <lucho870601@gmail.com>
2014-01-05 13:38:27 +0100

    refactor the code of the watering system

--- water_pump/water_pump.ino
+++ water_pump/water_pump.ino
@@ -5,6 +5,12 @@
  * Transistor changed to S9013 which has higher Ic and runs the water pump with higher rpm.
  */
 
+// MODIFY THESE VALUES:
+// change the periods between watering events and the duration of the watering event
+#define WATERING_DURATION_IN_SECONDS 20
+#define WATERING_PERIOD_IN_DAYS 1
+/////////////////////////////////////////////////////////////////////////////////////
+
 #include "LowPower.h"
 
 #define PUMP_PIN 3
@@ -12,29 +18,61 @@
 
 // measuring the actual time spent in sleep mode shows that it takes more than 8 sec.
 #define SLEEP_DURATION 9.35
-#define WATERING_DURATION (65 / SLEEP_DURATION) // seconds
-#define WATERING_PERIOD (( (3*60) / SLEEP_DURATION) - WATERING_DURATION) // (60 * 60 * 24 * 3) // seconds
+
+const unsigned long WATERING_DURATION_CYCLES = (unsigned long)WATERING_DURATION_IN_SECONDS / SLEEP_DURATION;
+const unsigned long WATERING_PERIOD_CYCLES = ((unsigned long)WATERING_PERIOD_IN_DAYS*24*60*60) / SLEEP_DURATION;
+boolean is_first_run = true;
 
 void setup () {
   pinMode(PUMP_PIN, OUTPUT);
   pinMode(LED_PIN, OUTPUT);
 }
 
-void loop() {
-  
-  digitalWrite(LED_PIN, LOW);
-  digitalWrite(PUMP_PIN, LOW);
-  
-  for (int period_counter = 0; period_counter < WATERING_PERIOD; period_counter++) {
+inline void sleep(unsigned long cycles) {
+  while (cycles > 0) {
     // Sleep for 8 s with ADC module and BOD module off
     LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+    cycles--;
   }
-  
+}
+
+inline void turn_on() {
   digitalWrite(LED_PIN, HIGH);
   digitalWrite(PUMP_PIN, HIGH);
+}
+
+inline void turn_off() {
+  digitalWrite(LED_PIN, LOW);
+  digitalWrite(PUMP_PIN, LOW);
+}
+
+void test_run() {
+  sleep(1);
+  turn_on();
+  sleep(1);
+  turn_off();
+}
+
+void loop() {
   
-  for (int duration_counter = 0; duration_counter < WATERING_DURATION; duration_counter++) {
-    // Sleep for 8 s with ADC module and BOD module off
-    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+  if (is_first_run) {
+    test_run();
+    is_first_run = false;
   }
+
+  sleep(WATERING_PERIOD_CYCLES);
+
+  // split watering in two, so that there is system cool down period inbetween
+  // first half of the watering
+  turn_on();
+  sleep((WATERING_DURATION_CYCLES + 1) / 2);
+  turn_off();
+
+  // 30 seconds system cool down
+  sleep(3);
+
+  // second half of the watering
+  turn_on();
+  sleep(WATERING_DURATION_CYCLES / 2);
+  turn_off();
 }
